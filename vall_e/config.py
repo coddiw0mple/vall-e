266ebb21bf13572c4f3d097c09208a04d2a2309e
Divith Phogat <coddiw0mpl4@gmail.com>
2023-02-26 12:53:19 +0530

    Update config.py

--- vall_e/config.py
+++ vall_e/config.py
@@ -25,7 +25,7 @@ class Config(ConfigBase):
     token_dim: int = 256
     num_tokens: int = 1024
 
-    nj: int = 8
+    nj: int = 2
     batch_size: int = 32
     eval_batch_size: int = 32
     warmup_min_lr: float = 1e-6
